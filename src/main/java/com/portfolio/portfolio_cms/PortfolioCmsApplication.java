0c1d2f2205d49f947f26cb10b389497376a1c3d0
Sirionss <siriosiolos@gmail.com>
2026-05-16 22:03:32 +0200

    Initial commit: Spring Boot project setup with PostgreSQL

---
+++ src/main/java/com/portfolio/portfolio_cms/PortfolioCmsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfolio_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioCmsApplication.class, args);
+	}
+
+}
