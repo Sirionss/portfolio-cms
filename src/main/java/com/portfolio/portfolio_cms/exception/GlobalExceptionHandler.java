eee6a8fd0881fcd831e3177e063ed4e5f056d680
Sirionss <siriosiolos@gmail.com>
2026-05-18 20:42:19 +0200

    fix typo

--- src/main/java/com/portfolio/portfolio_cms/exception/GlobalExceptionHandler.java
+++ src/main/java/com/portfolio/portfolio_cms/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
- add
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
